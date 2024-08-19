e914bf119cf353ba072996a0b7ed5220932998b0
nisha <nishaas.naveen@gmail.com>
2024-08-19 10:02:05 +0530

    initial commit

---
+++ list.py
@@ -0,0 +1,28 @@
+#python list are orderd colletion of data.
+#creating a python list
+
+List = [1, 2, 3, "nisha", 2.2]
+print(List)
+
+#creating the list with the use of multiple values
+
+List1 = ["my", "name", "is", "nisha"]
+print("list containing multiple values:")
+print(List1)
+
+#creating multi dimentional list
+
+List2 = [["my", "Father", "is"],["sadanandan"]]
+print("\nmuti dimentional list:")
+print(List2)
+
+
+#Accessing elemnt using index nmmber
+print("accessing element from the list")
+print(List1[3])
+print(List1[2])
+
+#accessing elemnt using negative index
+print("accessing element using negative indexing :")
+print(List2[-1])
+print(List2[-2])
