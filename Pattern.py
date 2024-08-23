836e711d683fd052fccc0d00197dd01066136f8f
nisha <nishaas.naveen@gmail.com>
2024-08-23 11:59:32 +0530

    pattern

---
+++ Pattern.py
@@ -0,0 +1,23 @@
+for i in range(4):
+   for j in range(4):
+      print("#",end=" ")
+
+   print()
+
+for a in range(4):
+   for b in range(a):
+      print("#",end=" ")
+
+   print()
+
+for i in range(4):
+   for j in range(i+1):
+      print("#",end=" ")
+
+   print()
+
+for i in range(4):
+   for j in range(4-i):
+      print("#",end=" ")
+
+   print()
